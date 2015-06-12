8ee66f08229cd89640f66d3fed8d295c07df8509
davidjmcgovern <davidjmcgovern@gmail.com>
2015-06-12 06:08:04 -0700

    Added Email TagHelper

---
+++ src/TagHelperSample/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace TagHelperSample.TagHelpers
+{
+    [TargetElement("email", Attributes = "domain")]
+    [TargetElement("email")]
+    public class EmailTagHelper : TagHelper
+    {
+        string emailDomain = "mycompany.com";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(context.GetChildContentAsync().Result.GetContent());
+
+            if (context.AllAttributes.ContainsKey("domain"))
+            {
+                sb.Append("@" + context.AllAttributes["domain"].ToString());
+            }
+            else
+            {
+                sb.Append("@" + emailDomain);
+            }
+
+            output.TagName = "a";
+            output.SelfClosing = false;
+            output.Attributes.Remove("domain");
+            output.Content.SetContent(sb.ToString());
+            output.Attributes["href"] = "mailto:" + output.Content;
+        }
+    }
+}
